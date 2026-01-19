2394eb363a58fd26902f5de5482eda5aa64167d0
kenctijzz <248065969+kenctijzz@users.noreply.github.com>
2026-01-19 22:55:43 +0300

    Fixed bug when user could not navigate to the same film page twice in a row

--- app/src/main/java/com/example/cinema/ui/navigation/NavigationManager.kt
+++ app/src/main/java/com/example/cinema/ui/navigation/NavigationManager.kt
@@ -1,24 +1,32 @@
 package com.example.cinema.ui.navigation
 
+import android.R.attr.tag
+import android.R.id.message
+import android.util.Log
+import android.util.Log.e
+import androidx.compose.runtime.rememberCoroutineScope
+import coil3.util.Logger
 import com.example.cinema.core.navigation.UiNavigation
+import com.example.cinema.ui.navigation.NavigationManager.currentScreen
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 
 
 object NavigationManager {
+
     private val _events = MutableSharedFlow<UiNavigation<Any>>(
         extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_LATEST
     )
     var currentScreen: Any = Screen.FilmList
+
     val events: SharedFlow<UiNavigation<Any>> = _events
     suspend fun navigateTo(route: Any) {
-        if (currentScreen == route) {
-            return
-        } else {
-            _events.emit(UiNavigation.NavigateTo(route))
-        }
-        currentScreen = route
+        _events.emit(UiNavigation.NavigateTo(route))
     }
 
     suspend fun navigateBack() {
