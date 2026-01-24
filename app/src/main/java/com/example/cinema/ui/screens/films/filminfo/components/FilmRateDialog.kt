4a3d68c415119eaf68263ab3aa8562b2092d3854
kenctijzz <248065969+kenctijzz@users.noreply.github.com>
2026-01-24 19:34:44 +0300

    Refactored FilmRateDialog from BottomSheet+LazyRow to BasicAlertDialog+LazyColumn

--- app/src/main/java/com/example/cinema/ui/screens/films/filminfo/components/FilmRateDialog.kt
+++ app/src/main/java/com/example/cinema/ui/screens/films/filminfo/components/FilmRateDialog.kt
@@ -1,10 +1,14 @@
 package com.example.cinema.ui.screens.films.filminfo.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,7 +19,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.cinema.core.ui.UiEvent
@@ -42,38 +49,48 @@ fun FilmRateDialog(
     var isSelectRate by remember { mutableStateOf(false) }
     var showValidText by remember { mutableStateOf(false) }
     var selectedRate by remember { mutableStateOf(-1) }
-    ModalBottomSheet(
+    BasicAlertDialog(
         onDismissRequest = {
             onDismiss()
+            isSelectRate = false
             selectedRate = -1
         },
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Card(
+            modifier = Modifier
+                .alpha(0.96f)
+                .size(height = if (!isSelectRate) 390.dp else 200.dp, width = 120.dp)
+                .clip(RoundedCornerShape(32.dp))
         ) {
-            if (!isSelectRate)
-                RatingSelectRow(
-                    rateNumbers = rateNumbers,
-                    showValidText = { showValidText = false },
-                    selectedRate = { newRate -> selectedRate = newRate },
-                    isSelectRate = { isSelectRate = true })
-            if (isSelectRate) {
-                Text(
-                    text = "$selectedRate",
-                    fontWeight = FontWeight.ExtraBold,
-                    fontSize = 64.sp,
-                    color = selectedRate.toDouble().toRatingColor()
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                if (!isSelectRate)
+                    RatingSelectColumn(
+                        rateNumbers = rateNumbers,
+                        showValidText = { showValidText = false },
+                        selectedRate = { newRate -> selectedRate = newRate },
+                        isSelectRate = { isSelectRate = true })
+
+                if (isSelectRate) {
+                    Text(
+                        text = "$selectedRate",
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 64.sp,
+                        color = selectedRate.toDouble().toRatingColor()
+                    )
+                }
+                if (showValidText) {
+                    Text("Choose rate", color = MaterialTheme.colorScheme.error)
+                }
+                FilmRateButton(
+                    selectedRate = selectedRate,
+                    showValidText = { newValue -> showValidText = newValue },
+                    isSelectRate = isSelectRate
                 )
             }
-            if (showValidText) {
-                Text("Choose rate", color = MaterialTheme.colorScheme.error)
-            }
-            FilmRateButton(
-                selectedRate = selectedRate,
-                showValidText = { newValue -> showValidText = newValue },
-                isSelectRate = isSelectRate
-            )
         }
     }
 }
